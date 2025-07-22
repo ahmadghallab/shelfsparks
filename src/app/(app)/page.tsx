b900d8469d8f93280a941d84db674d58e5cf496f
Ahmad Ghallab <help.ahmadghallab@gmail.com>
2025-07-22 20:11:09 +0300

    chore: padding adjustment

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -13,11 +13,11 @@ export default async function Home() {
         </h1>
       </div>
       <div className='relative before:absolute before:top-0 before:h-px before:w-[200vw] before:bg-gray-950/5 dark:before:bg-white/10 before:-left-[100vw] after:absolute after:bottom-0 after:h-px after:w-[200vw] after:bg-gray-950/5 dark:after:bg-white/10 after:-left-[100vw]'>
-        <p className='max-w-(--breakpoint-md) px-2 font-normal text-muted-foreground dark:text-gray-400'>
+        <p className='max-w-(--breakpoint-md) font-normal text-muted-foreground dark:text-gray-400'>
           The source is not available.
         </p>
       </div>
-      <div className='mt-4 sm:mt-10 sm:px-2 relative before:absolute before:top-0 before:h-px before:w-[200vw] before:bg-gray-950/5 dark:before:bg-white/10 before:-left-[100vw] after:absolute after:bottom-0 after:h-px after:w-[200vw] after:bg-gray-950/5 dark:after:bg-white/10 after:-left-[100vw] flex gap-3'>
+      <div className='mt-4 sm:mt-10 relative before:absolute before:top-0 before:h-px before:w-[200vw] before:bg-gray-950/5 dark:before:bg-white/10 before:-left-[100vw] after:absolute after:bottom-0 after:h-px after:w-[200vw] after:bg-gray-950/5 dark:after:bg-white/10 after:-left-[100vw] flex gap-3'>
         <ShuffleButton />
         <ShareButton />
       </div>
